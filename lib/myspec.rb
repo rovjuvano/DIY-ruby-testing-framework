0571ec4f72cb771fe27a7898173dd6bfc248a8b4
Robert Juliano <rovjuvano@gmail.com>
2013-11-01 23:44:15 -0400

    wrap in module

--- lib/myspec.rb
+++ lib/myspec.rb
@@ -1,11 +1,13 @@
-module DSL
-  def Then(&block)
-  begin
-    result = block.call
-    puts result ? 'pass' : 'fail'
-  rescue Exception => e
-    puts e
-  end
+module MySpec
+  module DSL
+    def Then(&block)
+    begin
+      result = block.call
+      puts result ? 'pass' : 'fail'
+    rescue Exception => e
+      puts e
+    end
+    end
   end
 end
-extend DSL
+extend MySpec::DSL
