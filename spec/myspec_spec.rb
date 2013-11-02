2d4e22faea13cab979c87386235395e58b0be481
Robert Juliano <rovjuvano@gmail.com>
2013-11-02 14:57:59 -0400

    Remove infinitely recursive definitions from test cases

--- spec/myspec_spec.rb
+++ spec/myspec_spec.rb
@@ -1,13 +1,13 @@
 describe 'Subject Under Test' do
   Given(:subject) { "I am Given" }
-  Then { @subject =~ /Given Two/ }
-  Then { @subject =~ /result/ }
+  Then { @result =~ /Given Two/ }
+  Then { @result =~ /result/ }
   Then { fail 'exception' }
-  When { @subject += " Two" }
+  When(:result) { @subject + " Two" }
   context 'when under conditions' do
     Given(:new_subject) { "I am nested Given" }
-    When { @new_subject += " Two" }
-    Then { @new_subject =~ /nested Given Two/ }
-    Then { @subject =~ /Given Two/ }
+    When(:new_result) { @new_subject + " Two" }
+    Then { @new_result =~ /nested Given Two/ }
+    Then { @result =~ /Given Two/ }
   end
 end
