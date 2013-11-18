dff25f7f524c196f1579f4490bda7d62f4fba9c1
Robert Juliano <rovjuvano@gmail.com>
2013-11-18 11:57:27 -0500

    Where is Then defined?

--- spec/myspec_spec.rb
+++ spec/myspec_spec.rb
@@ -1,1 +1,9 @@
 Then { puts "Hello World" }
+puts Object.public_method_defined?('Then') ? 'public' : 'not public'
+puts Object.private_method_defined?('Then') ? 'private' : 'not private'
+Object.send(:Then) { puts "Goodbye World" }
+begin
+  Object.Then { puts "And scene" }
+rescue Exception => e
+  puts e
+end
